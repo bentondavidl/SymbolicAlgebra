459ee75ef01ec10aab651491fae202f3d4b90786
David <bentondavidl@users.noreply.github.com>
2020-12-19 11:34:20 -0500

    Add documentation

--- exp_solver.py
+++ exp_solver.py
@@ -1,17 +1,28 @@
 from collections import deque
 
+
 class parser(object):
     """
-    Holds the methods for parsing an expression
+    Holds the methods for parsing an expression.
+    I know this could've been done with an eval(). That wasn't the point.
     """
     @staticmethod
     def parse(expression: str):
-        """
-        Take inputed expression and return a parse tree
+        """Convert the provided expression to Reverse Polish Notation so it can be handled more easily.
+
+        Parameters
+        ----------
+        expression : str
+            A mathamatical expression in the form of a string
+
+        Returns
+        -------
+        deque
+            The RPN as a queue for use in the `reverse_polish` method
         """
         output = deque()
         operator = deque()
-        ops = {'+':2,'-':2,'*':3,'/':3,'^':4}
+        ops = {'+': 2, '-': 2, '*': 3, '/': 3, '^': 4}
 
         for token in expression:
             if token in '0123456789':
@@ -20,8 +31,9 @@ class parser(object):
             #     operator.append(token)
             elif token in '+-*/^':
                 while len(operator) > 0 and operator[-1] in '+-*/^' and (
-                        ops[operator[-1]] > ops[token] or (ops[operator[-1]] > ops[token] and token == '^')
-                        ) and token != '(':
+                        ops[operator[-1]] > ops[token] or (
+                            ops[operator[-1]] > ops[token] and token == '^')
+                ) and token != '(':
                     output.appendleft(operator.pop())
                 operator.append(token)
             elif token == '(':
@@ -35,11 +47,23 @@ class parser(object):
                 #     output.appendleft(operator.pop())
         while len(operator) > 0:
             output.appendleft(operator.pop())
-        
+
         return output
 
     @staticmethod
     def reverse_polish(expression: deque) -> float:
+        """Computes the value of expresions provided in RPN
+
+        Parameters
+        ----------
+        expression : deque
+            Expression in RPN, typically from `parse` 
+
+        Returns
+        -------
+        float
+            The floating point value of the expression
+        """
         evaluation = []
 
         while len(expression) > 0:
@@ -47,7 +71,8 @@ class parser(object):
             if token in '0123456789':
                 evaluation.append(token)
             elif token in '+-*/^':
-                if token == '^': token = '**'
+                if token == '^':
+                    token = '**'
                 operands = [evaluation.pop(), evaluation.pop()]
                 evaluation.append(eval(f'{operands[1]} {token} {operands[0]}'))
 
